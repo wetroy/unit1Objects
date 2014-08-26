2a16ffad19c4de9aa94b9fdf63bba454d73e157b
wetroy <wetroy@student.naperville203.org>
2014-08-26 11:23:48 -0500

    E1.15, E1.17, and error programs
    beginning programs

---
+++ new2/DialogViewer.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class DialogViewer
+{
+    public static void main(String[] args)
+    {
+        String name = JOptionPane.showInputDialog("What is your name?");
+        System.out.print("\nHello ");
+        System.out.print(name);
+    }
+}
